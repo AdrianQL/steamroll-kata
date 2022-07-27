0bcac284f1412769f748062cb4f4f1dc2038e7bc
Adrián Quesada López <aquesada@hiberus.come>
2022-07-27 09:48:26 +0200

    feat: modify test suite

--- test/steamroll.spec.js
+++ test/steamroll.spec.js
@@ -1,17 +1,17 @@
 const steamroll = require('../steamroll');
 
 test('steamroll([[["a"]], [["b"]]])', () => {
-  expect(steamroll([[["a"]], [["b"]]])).toBe(["a", "b"]); 
+  expect(steamroll([[["a"]], [["b"]]])).toStrictEqual(["a", "b"]); 
 });
 
 test('steamrollArray([1, [2], [3, [[4]]]])', () => {
-  expect(steamroll([1, [2], [3, [[4]]]])).toBe([1, 2, 3, 4]);
+  expect(steamroll([1, [2], [3, [[4]]]])).toStrictEqual([1, 2, 3, 4]);
 });
 
 test('steamrollArray([1, [], [3, [[4]]]])', () => {
-  expect(steamroll([[["a"]], [["b"]]])).toBe([1, 3, 4]);
+  expect(steamroll([1, [], [3, [[4]]]])).toStrictEqual([1, 3, 4]);
 });
 
 test('steamrollArray([1, {}, [3, [[4]]]])', () => {
-  expect(steamroll([1, {}, [3, [[4]]]])).toBe([1, {}, 3, 4]);
+  expect(steamroll([1, {}, [3, [[4]]]])).toStrictEqual([1, {}, 3, 4]);
 });
